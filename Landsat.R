e1ff3483f93fac37b7a434842da02c7e409af794
jpgilbert1 <jean-philippe.gilbert.5@ulaval.ca>
2021-04-13 13:04:27 -0400

    MAJ

--- Landsat.R
+++ Landsat.R
@@ -123,5 +123,47 @@ multiplicative_rescaling <- multiplicative_rescaling$getInfo()
 additive_rescaling <- image_landsat$get('RADIANCE_ADD_BAND_10')
 additive_rescaling <- additive_rescaling$getInfo()
 
+nom_image <- image_landsat$get("system:index")
+nom_image <- nom_image$getInfo()
 
+for (i in 1:length(dictionnaire_image_qc))
+{
+  image_landsat <- ee$Image(dictionnaire_image_qc[i])
+  couvert_nuageux <- image_landsat$get('CLOUD_COVER')
+  couvert_nuageux <- couvert_nuageux$getInfo()
+  if(couvert_nuageux < 5)
+  {
+    print(couvert_nuageux)
+
+    nom_image <- image_landsat$get("system:index")
+    nom_image <- nom_image$getInfo()
+    
+    multiplicative_rescaling <- image_landsat$get('RADIANCE_MULT_BAND_10')
+    multiplicative_rescaling <- multiplicative_rescaling$getInfo()
+    
+    additive_rescaling <- image_landsat$get('RADIANCE_ADD_BAND_10')
+    additive_rescaling <- additive_rescaling$getInfo()
+    
+    task_img <- ee$batch$Export$image$toDrive(image = image_landsat,
+                                              region = secteur_ville_qc,
+                                              fileNamePrefix = dictionnaire_image_qc[i],
+                                              folder = 'Ville_de_qc_2016')
+    task_img$start()$
+    ee_monitoring(task_img)
+    
+    if (isFALSE(exists('info_image') && is.data.frame(get('info_image'))))
+    {
+      info_image <- data.frame(nom_image,multiplicative_rescaling, additive_rescaling, couvert_nuageux, stringsAsFactors=FALSE)
+    }else
+    {
+      info_image <- rbind(info_image, c(nom_image,multiplicative_rescaling, additive_rescaling, couvert_nuageux))
+    }
+  }
+}
+
+
+#avec ces informations, il est possible de calculer le TOA (quoiqu'il semble déjà fait par GEE, on peut comparer cette méthode avec)
+
+
+x <- data.frame(nom_image,multiplicative_rescaling, additive_rescaling, couvert_nuageux)
 
