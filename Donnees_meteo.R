f2e41e9be371b95535be215f121f0ac0f78eb018
jpgilbert1 <jean-philippe.gilbert.5@ulaval.ca>
2021-04-30 14:23:50 -0400

    MAJ

--- Donnees_meteo.R
+++ Donnees_meteo.R
@@ -27,8 +27,11 @@ for (i in 1:nrow(liste_station))
 }
 
 ##une fois que les données sont téléchargées, il faut déterminer quelles sont les dates des vagues de chaleur, pour aller chercher les Landsat adéquat
+#donc on se fit sur la definition de INSPQ : https://www.inspq.qc.ca/pdf/publications/1079_IndicateursVigieSanteChaleur.pdf
+#on on sort les dates 3 jours consecutf de journee chaude superieur a 30 et minimal superieur a 20.
+
 donnees_meteo <- list.files(path = '/Users/jean-philippegilbert/Documents/Université Laval/Cartographie vulnérabilité vagues de chaleur accamblante - General/Data/Donnees_meteo/Donnees_2015_2020/', full.names = TRUE)
-file <- read.csv(donnees_meteo[1])
+file <- read.csv(donnees_meteo[1]) 
 
 nb_de_jour =0
 fichier_vague_de_chaleur <- file[1,]
@@ -60,6 +63,45 @@ for (i in 1:length(donnees_meteo))
   }
 }
 
+fichier_vague_de_chaleur <- fichier_vague_de_chaleur[-1,]
 
 
+test <- merge(fichier_vague_de_chaleur, liste_station[,1:3], by.x = "ID.climatologique" , by.y = "X.1")
 
+
+#Ne donne pas beaucoup d'images, donc peut-etre prendre des images avec une température de jour en haut de 30 au lieu 
+#de prendre la définition de vagues de chaleur de INSPQ
+
+donnees_meteo <- list.files(path = '/Users/jean-philippegilbert/Documents/Université Laval/Cartographie vulnérabilité vagues de chaleur accamblante - General/Data/Donnees_meteo/Donnees_2015_2020/', full.names = TRUE)
+file <- read.csv(donnees_meteo[1]) 
+
+fichier_journees_chaudes <- file[1,]
+for (i in 1:length(donnees_meteo))
+{
+  file <- read.csv(donnees_meteo[i])
+  file <- na_if(file, "")
+  file$ID.climatologique <- as.character(file$ID.climatologique)
+  file2 <- merge(file, liste_station[,1:3], by.x = "Nom.de.la.Station" , by.y = "Modified.Date..2021.03.31.23.34.UTC")
+  if(file2$X == "QUEBEC")
+  {
+    for (j in 1:nrow(file))
+    {
+      if (file2$Mois[j] %in% 4:11 && !is.na(file2$Temp.max...C.[j]) && !is.na(file2$Temp.min...C.[j]))
+      {
+        if(as.numeric(sub(",",".",file2$Temp.max...C.[j])) >= 30)
+        {
+          print(file$Nom.de.la.Station[j])
+          print(file$Date.Heure[j])
+          print(file$Temp.max...C.[j])
+          print(i)
+          print(j)
+          fichier_journees_chaudes <- rbind(fichier_journees_chaudes, file[j,])
+        }
+      }
+    }
+  }
+  print(i)
+}
+
+fichier_journees_chaudes <- fichier_journees_chaudes[!duplicated(fichier_journees_chaudes$Date.Heure), ]
+fichier_journees_chaudes <- fichier_journees_chaudes[-1,]
