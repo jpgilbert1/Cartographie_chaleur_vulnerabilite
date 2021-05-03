8f1a6f40bf36bfc61c08326670a86b62319431a6
jpgilbert1 <jean-philippe.gilbert.5@ulaval.ca>
2021-05-03 14:32:54 -0400

    Changement boucle en fonction, plus clean ainsi

--- Manipulation_RS_data.R
+++ Manipulation_RS_data.R
@@ -5,23 +5,47 @@ library("raster")
 setwd('/Users/jean-philippegilbert/Documents/Université Laval/Cartographie vulnérabilité vagues de chaleur accamblante - General/Data/Donnee_GAIA_Artificial_impervenous_surface')
 Image_GAIA_ref <- raster("GAIA_1985_2018_46_-074.tif")
 
-setwd('/Users/jean-philippegilbert/Documents/Université Laval/Cartographie vulnérabilité vagues de chaleur accamblante - General/Data/Landsat_methode_GEE/GEE_QC_2015')
-load("Metadata/Metadata_2015.rda")
-j <- 0
-for (i in 1:nrow(df))
+fichier_moins_10 <- function(df)
 {
-  if(df$CLOUD_COVER[i] <= 10)
+  j <- 0
+  for (i in 1:nrow(df))
   {
-    if (j == 0)
+    if(df$CLOUD_COVER[i] <= 10)
     {
-      metadata_2015_cloud_cover_10 <- df[i,]
-      j <- j+1
-    }else
-    {
-      metadata_2015_cloud_cover_10 <- rbind(metadata_2015_cloud_cover_10, df[i,])
+      if (j == 0)
+      {
+        metadata_cloud_cover_10 <- df[i,]
+        j <- j+1
+      }else
+      {
+        metadata_cloud_cover_10 <- rbind(metadata_cloud_cover_10, df[i,])
+      }
     }
   }
+  return(metadata_cloud_cover_10)
 }
+
+reprojection_raster <- function(df)
+{
+  for(i in 1:nrow(df))
+  {
+    Image_Landsat <- raster(paste0(df$`system:index`[i], '.tif'))
+    Image_Landsat_reproject <- projectRaster(Image_Landsat,
+                                             crs = crs(Image_GAIA_ref),
+                                             res = res(Image_GAIA_ref))
+    writeRaster(Image_Landsat_reproject, paste0("Reproject/",df$`system:index`[i]), format = "GTiff")
+  }
+}
+
+
+
+#2015
+setwd('/Users/jean-philippegilbert/Documents/Université Laval/Cartographie vulnérabilité vagues de chaleur accamblante - General/Data/Landsat_methode_GEE/GEE_QC_2015')
+load("Metadata/Metadata_2015.rda")
+
+test <- fichier_moins_10(df)
+reprojection_raster(test)
+
 
 for(i in 1:nrow(metadata_2015_cloud_cover_10))
 {
@@ -30,6 +54,35 @@ for(i in 1:nrow(metadata_2015_cloud_cover_10))
                                            crs = crs(Image_GAIA_ref),
                                            res = res(Image_GAIA_ref))
   writeRaster(Image_Landsat_reproject, paste0("Reproject/",metadata_2015_cloud_cover_10$`system:index`[i]), format = "GTiff")
+}
+
+
+#2016
+setwd('/Users/jean-philippegilbert/Documents/Université Laval/Cartographie vulnérabilité vagues de chaleur accamblante - General/Data/Landsat_methode_GEE/GEE_QC_2016')
+load("Metadata/Metadata_2016.rda")
+j <- 0
+for (i in 1:nrow(df))
+{
+  if(df$CLOUD_COVER[i] <= 10)
+  {
+    if (j == 0)
+    {
+      metadata_2016_cloud_cover_10 <- df[i,]
+      j <- j+1
+    }else
+    {
+      metadata_2016_cloud_cover_10 <- rbind(metadata_2016_cloud_cover_10, df[i,])
+    }
+  }
+}
+
+for(i in 1:nrow(metadata_2016_cloud_cover_10))
+{
+  Image_Landsat <- raster(paste0(metadata_2016_cloud_cover_10$`system:index`[i], '.tif'))
+  Image_Landsat_reproject <- projectRaster(Image_Landsat,
+                                           crs = crs(Image_GAIA_ref),
+                                           res = res(Image_GAIA_ref))
+  writeRaster(Image_Landsat_reproject, paste0("Reproject/",metadata_2016_cloud_cover_10$`system:index`[i]), format = "GTiff")
 }
 
 
